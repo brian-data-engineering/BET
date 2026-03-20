6eeaf9c225d126ca39692830868ffda357fa6a70
Brian Kamau <kamaub369@gmail.com>
2026-03-20 03:55:08 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,12 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <BetProvider>
-      {/* The Head tag ensures Lucra has the correct title 
-        and scaling on mobile devices 
-      */}
       <Head>
         <title>Lucra | Sports Betting & Live Odds</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta name="description" content="Lucra - Real-time sports data and betting markets" />
+        {/* Adding a favicon makes your site look like a real brand */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
