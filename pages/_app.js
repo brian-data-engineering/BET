61c87567e7740195b48dc628691eb8bdce92fe0d
Brian Kamau <kamaub369@gmail.com>
2026-03-20 00:12:38 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { BetProvider } from '../context/BetContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <BetProvider>
+      {/* The Head tag ensures Lucra has the correct title 
+        and scaling on mobile devices 
+      */}
+      <Head>
+        <title>Lucra | Sports Betting & Live Odds</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="description" content="Lucra - Real-time sports data and betting markets" />
+      </Head>
+
+      <Component {...pageProps} />
+    </BetProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
